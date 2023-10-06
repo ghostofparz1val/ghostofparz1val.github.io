9162856c50f532b827f44d1287afbb15a41d6fa5
ghostofparz1val <146997549+ghostofparz1val@users.noreply.github.com>
2023-10-07 05:12:07 +0530

    Update bdc_index.js

--- bdc_index.js
+++ bdc_index.js
@@ -4,7 +4,7 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'Oct 05, 2023',
+  birthdate: 'Nov 19, 2023',
   name: 'Payal'
 };
 
